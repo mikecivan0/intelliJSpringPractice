be60ba02be2ebe1ebdae4d5eb7acd83058e202a9
Ivan Mikec <mikecivan0@gmail.com>
2021-09-12 17:30:12 +0200

    changed annotation

--- src/main/java/hr/mikec/intellijspringpractice/model/Book.java
+++ src/main/java/hr/mikec/intellijspringpractice/model/Book.java
@@ -11,12 +11,8 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+
     @ManyToMany
-    @JoinTable(
-            name = "author_book",
-            joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "author_id")
-    )
     private Set<Author> authors;
 
     public Book() {
