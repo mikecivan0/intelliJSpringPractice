11fd272c963d7477dda170102b4495369eb7ebf2
Ivan Mikec <mikecivan0@gmail.com>
2021-09-12 15:01:49 +0200

    added plain project from spring initializr

---
+++ src/main/java/hr/mikec/intellijspringpractice/IntelliJSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package hr.mikec.intellijspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliJSpringPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliJSpringPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/mikec/intellijspringpractice/IntelliJSpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.mikec.intellijspringpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliJSpringPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
